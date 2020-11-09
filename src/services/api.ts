d64505574adcabac07a0999b063205fbdced6f03
Heron Eto <41599309+heroneto@users.noreply.github.com>
2020-11-09 10:58:56 -0300

    parametrized api host

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'
+import path from 'path'
+import * as dotenv from 'dotenv'
+dotenv.config({path: path.resolve(__dirname, '..', '..', '.env')})
 
 const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: process.env.REACT_APP_API_HOST
 })
+
+console.log(process.env.REACT_APP_API_HOST)
 
 export default api
